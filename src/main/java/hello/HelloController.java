e18c59f08781b2dd0010d603690d0b7a081bdfb5
Maimilian Weinhart <maximilian.weinhart@jambit.com>
2018-04-25 12:28:03 +0200

    Echo server and hostname

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,21 +1,14 @@
 package hello;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
-    @Value("${local.server.port}")
-    private String serverPort;
-
-    @Value("${local.server.hostname}")
-    private String serverHostName;
-
     @RequestMapping("/")
     public String index() {
-        return "Whohooooo! Greetings from Jambit! (" + serverHostName + ":" + serverPort + ")";
+        return "Whohooooo! Greetings from Jambit!";
     }
 
 }
